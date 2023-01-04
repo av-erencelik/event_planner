6b2a2beb88ea73e5858bbc341f1c21097ab7f3c9
erencelik <erenmemo05@gmail.com>
2023-01-04 23:03:12 +0300

    created eventdetail page

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 import classes from "./Button.module.css";
 
 const Button = (props: { link: string; children: ReactNode[] }) => {
-  console.log(props.children);
   return (
     <Link href={props.link} className={classes.btn}>
       {props.children}
--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,5 +1,29 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/eventdetail/event-summary";
+import EventLogistics from "../../components/eventdetail/event-logistics";
+import EventContent from "../../components/eventdetail/event-content";
 const EventPage = () => {
-  return <div>EventPage</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId as string);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventPage;
